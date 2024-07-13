059189c7b35f1e327c9126f6a47aee72f5eebffe
Mehedi-Hassan-Patwary <md.mehedi.hassan.patwary.cse@gmail.com>
2024-07-13 00:39:40 -0700

    adding logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Display from './componenets/Display';
 import Buttonscontainer from './componenets/Buttonscontainer';
 import styles from'./App.module.css';
+import { useState } from 'react';
 
 function App() {
+  const [calVal,setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if(buttonText === 'C'){
+        setCalVal("");
+    }else if(buttonText === '='){
+
+      const result =eval(calVal);
+      setCalVal(result);
+
+    }else{
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+
   return (
     <>
       <div className={styles.calculator}>
-        <Display></Display>
+        <Display displayValue={calVal} ></Display>
 
-        <Buttonscontainer></Buttonscontainer>
+        <Buttonscontainer onButtonClick={onButtonClick}></Buttonscontainer>
 
       </div>
     </>
--- src/componenets/Buttonscontainer.jsx
+++ src/componenets/Buttonscontainer.jsx
@@ -1,18 +1,17 @@
 import styles from "./ButtonsContainer.module.css";
+import React from "react";
 
-
-const Buttonscontainer = () =>{
+const Buttonscontainer = ({onButtonClick}) =>{
     const buttonName = ['C' , '1' , '2' ,'+','3' ,'4','-','5','6','*','7','8','/','=','9','0','.'];
    
    return <>
         <div className={styles.buttonsContainer}>
-            {buttonName.map(buttonName => <button className={styles.button}>{buttonName}</button>
+            {buttonName.map((buttonName ,index) => <button 
+            key={index}
+            className={styles.button} 
+            onClick={() => onButtonClick(buttonName)} >{buttonName}</button>
          )}
             
-            
-            
-              
-              
         </div>
         </>
 
